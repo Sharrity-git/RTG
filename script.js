18b63c87ce46972c2cdb420e7e19849729da2f23
Sharrity-git <harritys3716@stuemail.gcc.mass.edu>
2024-12-24 02:03:28 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// Your SheetDB API URL
+const sheetdbUrl = 'https://sheetdb.io/api/v1/1f9euyp8jy7nk';
+
+// Fetch data from SheetDB
+fetch(sheetdbUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);  // This will log the array of objects from your sheet
+    displayCards(data); // Function to display your cards with data
+  })
+  .catch(error => {
+    console.error('Error fetching data from SheetDB:', error);
+  });
+
+// Function to display your data as cards
+function displayCards(data) {
+  const container = document.getElementById('cards-container'); // Your container element
+  container.innerHTML = ''; // Clear the container before adding new cards
+
+  data.forEach(item => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+      <p>${item['Venue']}</p>
+      <p>${item['City']}</p>
+      <p>${item['Time']}</p>
+      <p>${item['Date']}</p>
+    `;
+    container.appendChild(card);
+  });
+}
+
+fetch('https://sheetdb.io/api/v1/1f9euyp8jy7nk')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // This should log the data from your Google Sheet
+    // You can now work with the data and add it to your page
+  })
+  .catch(error => console.error('Error fetching data:', error));
